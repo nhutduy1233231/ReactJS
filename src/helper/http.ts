6b8dec2ce4da081143e946b1caf4de7a019c8cd9
unknown <nhutduy1233231@gmail.com>
2025-02-13 21:01:19 +0700

    react router

---
+++ src/helper/http.ts
@@ -0,0 +1,74 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import { envSettings } from '~/helper/settings'
+
+type CustomOptions = Omit<RequestInit, 'method'> & { baseUrl?: string | undefined }
+
+export class HttpError extends Error {
+  status: number
+  payload: { message: string; [key: string]: any }
+
+  constructor({ status, payload }: { status: number; payload: any }) {
+    super('Http Error')
+    this.status = status
+    this.payload = payload
+  }
+}
+
+const request = async <Response>(
+  method: 'GET' | 'POST' | 'PUT' | 'DELETE',
+  url: string,
+  options?: CustomOptions | undefined
+) => {
+  let body: FormData | string | undefined = undefined
+  if (options?.body instanceof FormData) {
+    body = options.body
+  } else if (options?.body) {
+    body = JSON.stringify(options.body)
+  }
+
+  const baseHeaders: { [key: string]: string } = body instanceof FormData ? {} : { 'Content-Type': 'application/json' }
+
+  const accessToken = localStorage.getItem('access_token')
+  if (accessToken) {
+    baseHeaders.Authorization = `Bearer ${accessToken}`
+  }
+
+  const baseUrl = options?.baseUrl === undefined ? envSettings.public_api : options.baseUrl
+
+  const fullUrl = url.startsWith('/') ? `${baseUrl}${url}` : `${baseUrl}/${url}`
+
+  const res = await fetch(fullUrl, {
+    ...options,
+    headers: { ...baseHeaders, ...options?.headers } as any,
+    body,
+    method
+  })
+
+  const payload: Response = await res.json()
+
+  const data = { status: res.status, payload }
+
+  if (!res.ok) {
+    throw new HttpError(data)
+  }
+
+  return data
+}
+
+const http = {
+  get<Response>(url: string, options?: Omit<CustomOptions, 'body'> | undefined) {
+    return request<Response>('GET', url, options)
+  },
+  post<Response>(url: string, body: any, options?: Omit<CustomOptions, 'body'> | undefined) {
+    return request<Response>('POST', url, { ...options, body })
+  },
+  put<Response>(url: string, body: any, options?: Omit<CustomOptions, 'body'> | undefined) {
+    return request<Response>('PUT', url, { ...options, body })
+  },
+  delete<Response>(url: string, options?: Omit<CustomOptions, 'body'> | undefined) {
+    return request<Response>('DELETE', url, { ...options })
+  }
+}
+
+export default http
--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,23 @@ import '~styles/global.scss'
 
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Route, BrowserRouter as Router, Routes } from 'react-router'
+import PublicLayout from '~/app/layout/public'
+import { envSettings } from '~/helper/settings'
+
+const EXAMPLE_ROUTER = [
+  { path: '/', element: <PublicLayout>index</PublicLayout> },
+  { path: '/admin', element: <PublicLayout>admin</PublicLayout> }
+]
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <>Hello ReactJS</>
+    <Router basename={envSettings.baseUrl}>
+      <Routes>
+        {EXAMPLE_ROUTER.map((el) => {
+          return <Route key={el.path} path={el.path} element={el.element} />
+        })}
+      </Routes>
+    </Router>
   </React.StrictMode>
 )
